5ef24db89958a127e52b53eff4c56387db6604f6
aliciagouveia <aliciagouveia99@gmail.com>
2023-11-17 14:07:49 +0000

    initial commit

--- testhaversine.py
+++ testhaversine.py
@@ -1,46 +1,44 @@
 
-from math import radians, sin, cos, sqrt, atan2
-
-def haversine(lat1, lon1, lat2, lon2):
-    # Radius of the Earth in kilometers
-    R = 6371.0
-
-    # Convert latitude and longitude from degrees to radians
-    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
-
-    # Calculate the differences in coordinates
-    dlat = lat2 - lat1
-    dlon = lon2 - lon1
-
-    # Haversine formula
-    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
-    c = 2 * atan2(sqrt(a), sqrt(1 - a))
-
-    # Calculate the distance
-    distance = R * c
-
-    return distance
+from myfunctions import mysqrt, mysin, mycos, myarcsin, sin45, cos45
+from myhaversine import myhaversine
+from haversine import haversine
+import math
 
 def main():
-    # Input coordinates of the first point
-    lat1 = float(input("Enter the latitude of the first point: "))
-    lon1 = float(input("Enter the longitude of the first point: "))
+    test_mysqrt()
+    test_mysin()
+    test_mycos()
+    test_myarcsin()
+    test_myhaversine()
 
-    # Input coordinates of the second point
-    lat2 = float(input("Enter the latitude of the second point: "))
-    lon2 = float(input("Enter the longitude of the second point: "))
+def test_mysqrt():
+    x=2 #x apenas igual ou superior a 0 pois, se x menor que 0, o return é logo 0
+    assert round(mysqrt(x), 2) == round(x**0.5, 2)
 
-    # Calculate and display the distance
-    distance = haversine(lat1, lon1, lat2, lon2)
-    print(f"The distance between the two points is {distance:.2f} km.")
+def test_mysin():
+    assert mysin(30) == sin45(30)
+    assert mysin(50) == cos45(90-50)
+    assert mysin(100) == mysin(180-100)
+    assert mysin(200) == -mysin(200-180)
 
-    # Calculate coordinates with a 100m difference
-    lat2_new = lat2 + 0.0009  # 100m is approximately 0.0009 degrees in latitude
-    lon2_new = lon2 + 0.0009  # 100m is approximately 0.0009 degrees in longitude
+def test_mycos():
+    assert mycos(30) == cos45(30)
+    assert mycos(50) == sin45(90-50)
+    assert mycos(100) == -mycos(180-100)
+    assert mycos(200) == -mycos(200-180)
 
-    # Calculate and display the new distance
-    new_distance = haversine(lat1, lon1, lat2_new, lon2_new)
-    print(f"The new distance with a 100m difference is {new_distance:.2f} km.")
+def test_myarcsin():
+    x=0.5 #x está logo definido para estar compreendido entre -1 e 1
+    assert round(myarcsin(x), 2) == round(math.degrees(math.asin(x)), 2)
+
+def test_myhaversine():
+    tol = 0.1 #100 meters
+    p1=90,-100
+    p2=50,-180
+    my_distance = myhaversine(p1,p2)
+    correct_distance = haversine(p1,p2)
+    assert round(my_distance - correct_distance, 0) <= tol
+
 
 if __name__ == "__main__":
-    main()
+    main() 
