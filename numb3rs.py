0af82fb54a4daaa747b0c4b36b475d53747d2d84
aliciagouveia <aliciagouveia99@gmail.com>
2023-12-01 12:05:18 +0000

    innitial commit

--- numb3rs.py
+++ numb3rs.py
@@ -8,8 +8,8 @@ def main():
 
 def validate(ip):
     if re.search (r"^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$", ip):   #in the search we need to set the statement and which variable we're looking for(in this case the ip) 
-        list_of_numbers = ip.split(".") #os valores do IP serão incorporados numa lista e divididos por pontos
-        for number in list_of_numbers: #para cada número na lista só pode estar entre o range de 0 a 255
+        list_of_ipnumbers = ip.split(".") #os valores do IP serão incorporados numa lista e divididos por pontos
+        for number in list_of_ipnumbers: #para cada número na lista só pode estar entre o range de 0 a 255
             if not (0 <= int(number) <= 255) :
                 return False
         return True
